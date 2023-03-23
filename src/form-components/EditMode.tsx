ebc2cc68c44e475c78e9c5b263e3d5378ee9352f
rrachelr <rachrobins1@gmail.com>
2023-03-23 12:40:54 -0400

    finished edit mode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,54 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const [userName, setUserName] = useState<string>("Your Name");
+    const [student, setStudent] = useState<boolean>(true);
+    const text = student
+        ? userName + " is a student"
+        : userName + " is not a student";
+    function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
+        setEditMode(event.target.checked);
+    }
+    function updateUserName(event: React.ChangeEvent<HTMLInputElement>) {
+        setUserName(event.target.value);
+    }
+    function updateStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setStudent(event.target.checked);
+    }
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="is-edit-mode"
+                label="edit mode"
+                checked={editMode}
+                onChange={updateEditMode}
+            />
+            <div>
+                {editMode ? (
+                    <Form.Group controlId="formUserName">
+                        <Form.Label>Name:</Form.Label>
+                        <Form.Control
+                            value={userName}
+                            onChange={updateUserName}
+                            disabled={!editMode}
+                        />
+                    </Form.Group>
+                ) : null}
+                {editMode ? (
+                    <Form.Check
+                        type="checkbox"
+                        id="is-student-check"
+                        label="Student?"
+                        checked={student}
+                        onChange={updateStudent}
+                    />
+                ) : null}
+            </div>
+            {!editMode && text}
         </div>
     );
 }
